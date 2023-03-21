00a307d9f42fbfbb6c9077bd2f8132d614e84fb1
FadyAmir223 <fadyamir223@gmail.com>
2023-03-21 17:57:15 +0200

    before main in middle

--- src/routes/navbar.component.tsx
+++ src/routes/navbar.component.tsx
@@ -64,9 +64,69 @@ const Navbar = () => {
   );
 };
 
+const Sidebar = () => {
+  const { isSidebarOpen, setIsSidebarOpen } = useContext(SidebarContext);
+
+  return (
+    <>
+      <aside
+        className={`fixed z-50 grid gap-y-10 top-0 w-96 h-screen pt-5 pb-7 bg-stone-900 duration-500 ${
+          isSidebarOpen ? 'left-0' : '-left-96'
+        }`}
+        style={{ gridTemplateRows: `auto 1fr auto` }}
+      >
+        <div className="flex justify-between items-center px-5">
+          <span className="">LOGO</span>
+          <FaTimes
+            className="inline-block scale-150 text-red-600 cursor-pointer"
+            onClick={() => setIsSidebarOpen(false)}
+          />
+        </div>
+        <nav>
+          {sidebarLinks.map(({ icon, text }) => (
+            <Link
+              key={text}
+              to={text}
+              className="px-5 py-3 bg-stone-100/0 hover:bg-stone-100/100 capitalize text-xl block duration-700 text-gray-400"
+            >
+              <span className="mr-3 inline-block scale-125">{icon}</span>
+              {text}
+            </Link>
+          ))}
+        </nav>
+        <div className="flex justify-center gap-x-7">
+          {contactSocial.map(({ icon, path }) => (
+            <a
+              key={path}
+              href={path}
+              className="text-blue-400 scale-150  duration-150 hover:text-blue-300"
+            >
+              {icon}
+            </a>
+          ))}
+        </div>
+      </aside>
+
+      <div
+        className={`relative duration-300 ${
+          isSidebarOpen ? 'left-96' : 'left-0'
+        }`}
+        style={{ width: `calc(100% - ${isSidebarOpen ? '24rem' : '0px'})` }}
+      >
+        <Header />
+        <main
+          className="container mx-auto px-6 grid place-items-center"
+          style={{ minHeight: `calc(100vh - 5rem)` }}
+        >
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+};
+
 const ModalPage = () => {
   const [showModal, setShowModal] = useState(false);
-  const { isSidebarOpen } = useContext(SidebarContext);
 
   const setShowModal_ = () => {
     setShowModal((prevShowModal) => !prevShowModal);
@@ -74,25 +134,17 @@ const ModalPage = () => {
 
   return (
     <>
-      <div
-        className={`relative duration-300 ${
-          isSidebarOpen ? 'left-96' : 'left-0'
-        }`}
-        style={{ width: `calc(100% - ${isSidebarOpen ? '24rem' : null})` }}
+      <main
+        className="container mx-auto px-6 grid place-items-center"
+        style={{ minHeight: `calc(100vh - 5rem)` }}
       >
-        <Header />
-        <main
-          className="container mx-auto px-6 grid place-items-center"
-          style={{ minHeight: `calc(100vh - 5rem)` }}
+        <button
+          className=" capitalize text-sm px-2 py-1 bg-blue-300 hover:bg-blue-400 rounded-sm duration-200 tracking-wider"
+          onClick={setShowModal_}
         >
-          <button
-            className=" capitalize text-sm px-2 py-1 bg-blue-300 hover:bg-blue-400 rounded-sm duration-200 tracking-wider"
-            onClick={setShowModal_}
-          >
-            show modal
-          </button>
-        </main>
-      </div>
+          show modal
+        </button>
+      </main>
 
       <Modal>
         <div
@@ -135,55 +187,6 @@ const Header = () => {
         />
       )}
     </header>
-  );
-};
-
-const Sidebar = () => {
-  const { isSidebarOpen, setIsSidebarOpen } = useContext(SidebarContext);
-
-  return (
-    <>
-      <div className="">
-        <aside
-          className={`fixed z-50 grid gap-y-10 top-0 w-96 h-screen pt-5 pb-7 bg-stone-900 duration-500 ${
-            isSidebarOpen ? 'left-0' : '-left-96'
-          }`}
-          style={{ gridTemplateRows: `auto 1fr auto` }}
-        >
-          <div className="flex justify-between items-center px-5">
-            <span className="">LOGO</span>
-            <FaTimes
-              className="inline-block scale-150 text-red-600 cursor-pointer"
-              onClick={() => setIsSidebarOpen(false)}
-            />
-          </div>
-          <nav>
-            {sidebarLinks.map(({ icon, text }) => (
-              <Link
-                key={text}
-                to={text}
-                className="px-5 py-3 bg-stone-100/0 hover:bg-stone-100/100 capitalize text-xl block duration-700 text-gray-400"
-              >
-                <span className="mr-3 inline-block scale-125">{icon}</span>
-                {text}
-              </Link>
-            ))}
-          </nav>
-          <div className="flex justify-center gap-x-7">
-            {contactSocial.map(({ icon, path }) => (
-              <a
-                key={path}
-                href={path}
-                className="text-blue-400 scale-150  duration-150 hover:text-blue-300"
-              >
-                {icon}
-              </a>
-            ))}
-          </div>
-        </aside>
-      </div>
-      <Outlet />
-    </>
   );
 };
 
